e4868e3bbfab34e65bf65d05a4b39fffc6a61342
David White <david.rhys.white@icloud.com>
2020-08-30 13:10:42 +0100

    Added unit and integration tests for MidgardAPI.getStats method

--- test/datasources/midgard.integration.ts
+++ test/datasources/midgard.integration.ts
@@ -1,3 +1,31 @@
-test('getStats makes HTTP request to /v1/stats', () => {
-  // Test
+import nock from 'nock';
+import MidgardAPI from '../../src/datasources/midgard';
+
+const statsObject = {
+  dailyActiveUsers: 'dailyActiveUsers',
+  dailyTx: 'dailyTx',
+  monthlyActiveUsers: 'monthlyActiveUsers',
+  monthlyTx: 'monthlyTx',
+  poolCount: 'poolCount',
+  totalAssetBuys: 'totalAssetBuys',
+  totalAssetSells: 'totalAssetSells',
+  totalDepth: 'totalDepth',
+  totalEarned: 'totalEarned',
+  totalStakeTx: 'totalStakeTx',
+  totalStaked: 'totalStaked',
+  totalTx: 'totalTx',
+  totalUsers: 'totalUsers',
+  totalVolume: 'totalVolume',
+  totalVolume24hr: 'totalVolume24hr',
+  totalWithdrawTx: 'totalWithdrawTx',
+};
+
+test('getStats makes HTTP request to /v1/stats and returns object', async () => {
+  nock('http://midgard.hostname.local').get('/stats/path').reply(200, statsObject);
+  const midgard = new MidgardAPI();
+  midgard.initialize({ context: {}, cache: undefined });
+
+  const getStatsValue = await midgard.getStats();
+
+  expect(getStatsValue).toStrictEqual(statsObject);
 });
--- test/datasources/midgard.unit.ts
+++ test/datasources/midgard.unit.ts
@@ -1,3 +1,42 @@
-test('getStats makes HTTP request to /v1/stats', () => {
-  // Test
+import MidgardAPI from '../../src/datasources/midgard';
+
+const mockRestGet = jest.fn(() => 'mockRestGet');
+jest.mock('apollo-datasource-rest', () => {
+  class MockRESTDataSource {
+    baseUrl = '';
+    get = mockRestGet;
+  }
+
+  return {
+    RESTDataSource: MockRESTDataSource,
+  };
+});
+
+test('sets baseURL to settings.hostname', async () => {
+  const midgard = new MidgardAPI();
+
+  expect(midgard.baseURL).toBe('http://midgard.hostname.local');
+});
+
+test('sets settings to config.services.midgard', async () => {
+  const midgard = new MidgardAPI();
+
+  expect(midgard.settings).toStrictEqual({
+    hostname: 'http://midgard.hostname.local',
+    paths: {
+      stats: '/stats/path',
+      pools: '/pools/path',
+      poolsDetail: '/pools/detail/path',
+    },
+  });
+});
+
+test('getStats calls this.get with settings.paths.stats and returns value', async () => {
+  const midgard = new MidgardAPI();
+
+  const getStatsValue = await midgard.getStats();
+
+  expect(mockRestGet).toBeCalledTimes(1);
+  expect(mockRestGet).toBeCalledWith('/stats/path');
+  expect(getStatsValue).toBe('mockRestGet');
 });
