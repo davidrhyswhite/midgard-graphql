15783c8cf47cdb773f7318d8bf9e7bfd8146b501
David White <david.rhys.white@icloud.com>
2020-09-10 20:59:43 +0100

    Used new queries in e2e tests

--- test/e2e/transactions.test.ts
+++ test/e2e/transactions.test.ts
@@ -1,35 +1,11 @@
 import { createTestClient } from 'apollo-server-testing';
 import { ApolloServer } from 'apollo-server';
-import gql from 'graphql-tag';
 import nock from 'nock';
 import { createTransactions } from '../helpers/mock-creators';
 import { createServer } from '../../src/app';
+import { importQuery } from '../helpers/import-query';
 
-const GET_TRANSACTIONS = gql`
-  fragment transaction on Transaction {
-    txID
-    address
-  }
-  query getTransactions {
-    transactions(limit: 20, offset: 0) {
-      totalCount
-      nodes {
-        type
-        in {
-          ...transaction
-        }
-        out {
-          ...transaction
-        }
-      }
-      pageInfo {
-        nextCursor
-        endCursor
-        hasNextPage
-      }
-    }
-  }
-`;
+const TRANSACTIONS_QUERY = importQuery('transactions.graphql');
 
 test('returns an TransactionConnection type from application', async () => {
   const { query } = createTestClient(createServer(ApolloServer));
@@ -40,6 +16,6 @@ test('returns an TransactionConnection type from application', async () => {
     .reply(200, createTransactions());
 
   // run query against the server and snapshot the output
-  const res = await query({ query: GET_TRANSACTIONS });
+  const res = await query({ query: TRANSACTIONS_QUERY, variables: { limit: 20, offset: 0 } });
   expect(res).toMatchSnapshot();
 });
