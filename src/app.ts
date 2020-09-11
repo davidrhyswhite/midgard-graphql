65d33102eb8e74e65620290ed019ef9da5bb4c3b
David White <david.rhys.white@icloud.com>
2020-09-11 18:43:46 +0100

    Added .graphql-config to Apollo app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import { Config } from 'apollo-server-core';
 import { ApolloServer as Server } from 'apollo-server';
 import { ApolloServer as Lambda } from 'apollo-server-lambda';
+import { loadConfigSync } from 'graphql-config';
 import schema from './schema';
 import MidgardAPI from './datasources/midgard';
 import { DataSources } from './types/context';
+
+const config = loadConfigSync({ rootDir: './' });
 
 interface ApolloConstructable<T> {
   new (args: Config): T;
@@ -13,7 +16,9 @@ type ServerType = Server | Lambda;
 export function createServer(App: ApolloConstructable<ServerType>): ServerType {
   return new App({
     schema,
-    playground: true,
+    playground: {
+      config,
+    },
     introspection: true,
     dataSources: (): DataSources => ({
       midgardAPI: new MidgardAPI(),
